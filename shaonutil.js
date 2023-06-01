92476cab92cd3e66836bb57253fbb882e7865bf1
Shaon Majumder <smazoomder@gmail.com>
2023-06-01 23:58:41 +0600

    converted to ES6 syntax

--- shaonutil.js
+++ shaonutil.js
@@ -1,24 +1,24 @@
-function getElementByXPath(path) {
+const getElementByXPath = (path) => {
   return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-}
+};
 
-function getElementsByXPath(xpathToExecute){
-  var result = [];
-  var nodesSnapshot = document.evaluate(xpathToExecute, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null );
-  for ( var i=0 ; i < nodesSnapshot.snapshotLength; i++ ){
-    result.push( nodesSnapshot.snapshotItem(i) );
-  }
-  return result;
-}
+const getElementsByXPath = (xpathToExecute) => {
+	const result = [];
+	const nodesSnapshot = document.evaluate(xpathToExecute, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+	for (let i = 0; i < nodesSnapshot.snapshotLength; i++) {
+	  result.push(nodesSnapshot.snapshotItem(i));
+	}
+	return result;
+};
 
-function ChangeHTMLText(){
-	var XPATH = prompt("Please enter your Xpath", "");
-	var Message = prompt("Please enter your messaege", "");
+const changeHTMLText = () => {
+	const XPATH = prompt("Please enter your XPath", "");
+	const message = prompt("Please enter your message", "");
 
 	if ((XPATH == null || XPATH == "") && (Message == null || Message == "")) {
-	  txt = "User cancelled the prompt.";
-	  alert("cancled !!!");
+		txt = "User cancelled the prompt.";
+		alert("Canceled!");
 	} else {
-	  getElementByXPath(XPATH).innerHTML = Message;
+		getElementByXPath(XPATH).innerHTML = message;
 	}
-}
+};
